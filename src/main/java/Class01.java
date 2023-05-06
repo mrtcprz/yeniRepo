e68fcc2a5503a91ec978ab7eac0875c79b4beb82
murat çapraz <murathancapraz@gmail.com>
2023-05-06 21:45:25 +0300

    v3

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -2,6 +2,8 @@ public class Class01 {
     public static void main(String[] args) {
 
     //v1 calismalar...
+        //v2 uzerine calismalar
+        System.out.println("v3 kodlari..."); //v3
 
 
 
